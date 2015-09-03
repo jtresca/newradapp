df8f1d572bf032237a61469dac3ae649789a5f72
William Countiss <william.countiss@gmail.com>
2015-09-03 17:37:27 -0400

    I like having the strict mode inside of functions when I can. just personal preference

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -1,7 +1,8 @@
-'use strict';
+
 
 angular.module('radAppApp')
   .config(['$routeProvider', function ($routeProvider) {
+  	'use strict';
     $routeProvider
       .when('/', {
       	templateUrl: 'app/account/login/login.html',
