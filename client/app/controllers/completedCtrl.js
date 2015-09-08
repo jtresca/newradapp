0b61549219c48aab25241291c9c02fcd2a57eb9b
William Countiss <william.countiss@gmail.com>
2015-09-08 17:26:18 -0400

    cleaning up the completed.html a bit. will display % value for the exam. also displays the students name. still looks like crap, but I'm just working on functionality right now.

--- client/app/controllers/completedCtrl.js
+++ client/app/controllers/completedCtrl.js
@@ -2,10 +2,14 @@
  * Created by william on 9/7/15.
  */
 angular.module('radAppApp')
-  .controller('completedCtrl', ['$scope', 'formFactory', function ($scope, formFactory) {
+  .controller('completedCtrl', ['$scope', 'formFactory', '$location', function ($scope, formFactory, $location) {
     var ff = formFactory;
     var vm = this;
     $scope.completedExam = ff.completedExam;
+    $scope.beginNewExam = function () {
+
+      $location.path('/exam');
+    };
 
 
   }]);
